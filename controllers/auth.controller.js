1eda721bdefe5d4761eb8ec28f489e766b0e1650
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-02-05 16:43:53 -0800

    🤫

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 /** Authorization routes here */
 const jwt = require('jsonwebtoken');
-const User = require('../models/user.model');
+const User = require('../models/user');
 
 module.exports = (app) => {
     // SIGN-UP GET
