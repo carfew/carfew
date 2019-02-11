cd8f838968e8443b9172e4629ea21c7c38f2de33
luxury <luxury@luxury.local>
2019-02-10 22:59:48 -0800

    finished with design for login

--- config/express.js
+++ config/express.js
@@ -5,13 +5,6 @@ const config = require('./config');
 
 const app = express();
 
-/* if handlebars doesn't work or whatever uncomment below */
-
-// var exphbs = require('express-handlebars');
-
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
-
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
