1005a3a9079448050c7097af21103becec75d908
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-03-09 17:24:45 -0800

    🔘 fixed submit button

---
+++ carfew-react/build/precache-manifest.9d932a4ddbd7b90791457f0323da133e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "100baea3642dbb137a4f",
+    "url": "/static/js/main.100baea3.chunk.js"
+  },
+  {
+    "revision": "787819828d4c59d0ed16",
+    "url": "/static/js/2.78781982.chunk.js"
+  },
+  {
+    "revision": "100baea3642dbb137a4f",
+    "url": "/static/css/main.17b3cdc7.chunk.css"
+  },
+  {
+    "revision": "cbc0e0d9c6707fc25cc6be2adeacff5c",
+    "url": "/index.html"
+  }
+];
